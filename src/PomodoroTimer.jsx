0172db88fc9205cb8b02f3ce79191a385cd8a517
Ayman Benchamkha <69921106+chigivera@users.noreply.github.com>
2024-05-12 18:58:29 +0000

    Update PomodoroTimer.jsx

--- src/PomodoroTimer.jsx
+++ src/PomodoroTimer.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Box, Button, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import { decrementBreak, incrementBreak, resetBreak } from './state/break/breakReducer';
 import { decrementSession, incrementSession, resetSession } from './state/session/sessionReducer';
 import styled from 'styled-components';
